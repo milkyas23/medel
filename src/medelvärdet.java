1e16b8c9445b183cd6d7513f18fa541d80020ace
MilkyasEyob.Habte <milkyas23>
2019-11-21 11:10:52 +0100

    Initial commit

---
+++ src/medelvärdet.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class medelvärdet {
+    public class medelvärdet {
+        public static void main (String[] args) {
+            Scanner in = new Scanner( )
+            System.out.println( "Hur många tal vil du ha?");
+
+
+
+        }
+    }
+}
